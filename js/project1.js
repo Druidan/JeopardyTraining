550adf67a5b5ffc1238e7bdf2cc75e08c11e06c5
Druidan <44009123+Druidan@users.noreply.github.com>
2019-03-13 20:34:05 -0500

    Got the basic jservice api up and running

--- js/project1.js
+++ js/project1.js
@@ -1,5 +1,104 @@
 $(document).ready(function(){    //My JS starts past this point.
- 
+
+
+used100Questions = [];
+used200Questions = [];
+used300Questions = [];
+used400Questions = [];
+used500Questions = [];
+used600Questions = [];
+used700Questions = [];
+used800Questions = [];
+used900Questions = [];
+used1000Questions = [];
+
+const currentQuestions = {
+
+}
+
+//JService API Query
+    let queryInterval = 1;
+    let offsetInterval = 0;
+    let qValue = queryInterval * 100;
+    let targetArray;
+
+function fillAllQs(){
+    for (var i = 0; i < 10 ; i++){
+        grabQuestions()
+        queryInterval++
+        console.log(queryInterval)
+    }
+    queryInterval=1;
+    console.log(currentQuestions);
+}
+fillAllQs();
+
+//API Functions
+function grabQuestions(){
+    if(qValue === 100){
+        targetArray = used100Questions;
+    } else {
+        if(qValue === 200){
+            targetArray = used200Questions;
+        } else {
+            if(qValue === 300){
+                targetArray = used300Questions;
+            } else {
+                if(qValue === 400){
+                    targetArray = used400Questions;
+                } else {
+                    if(qValue === 500){
+                        targetArray = used500Questions;
+                    } else {
+                        if(qValue === 600){
+                            targetArray = used600Questions;
+                        } else {
+                            if(qValue === 700){
+                                targetArray = used700Questions;
+                            } else {
+                                if(qValue === 800){
+                                    targetArray = used800Questions;
+                                } else {
+                                    if(qValue === 900){
+                                        targetArray = used900Questions;
+                                    } else {
+                                        if(qValue === 1000){
+                                            targetArray = used1000Questions;
+                                        } else {
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //Determine the offset for the jservice search query.
+    if(targetArray === 96){
+        offsetInterval++
+    };
+    const queryURL = "http://jservice.io/api/clues/?value=" + qValue + "&&offset=" + offsetInterval;
+    $.get(queryURL, function(response){
+        for (var j = 1 ; j <= 6; j++){
+            randomQ = response[Math.floor(Math.random() * 100)];
+            qObjName = "val"+qValue+"Num"+j;
+            tempQ = {};
+            tempQ[qObjName] = {
+                id: randomQ.id,
+                answer: randomQ.answer,
+                question: randomQ.question,
+                value: randomQ.value,
+                airdate: randomQ.airdate,
+                category: randomQ.category.title,
+            }
+            $.extend(currentQuestions, tempQ);
+            targetArray.push(randomQ.id)
+        }
+        });
+    };
 
 //Constructors and Prototypes
 function sound(src) {

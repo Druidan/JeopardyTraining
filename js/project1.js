53ba9145a8f99e112006d4fb5d5937c12e977120
Druidan <44009123+Druidan@users.noreply.github.com>
2019-03-14 12:55:22 -0500

    The API to grab the questions from jService is done! It does it completely properly and fills up the questions object!

--- js/project1.js
+++ js/project1.js
@@ -7,9 +7,7 @@ used300Questions = [];
 used400Questions = [];
 used500Questions = [];
 used600Questions = [];
-used700Questions = [];
 used800Questions = [];
-used900Questions = [];
 used1000Questions = [];
 
 const currentQuestions = {
@@ -19,29 +17,20 @@ const currentQuestions = {
     400 : {},
     500 : {},
     600 : {},
-    700 : {},
     800 : {},
-    900 : {},
     1000 : {},
 }
 
 //JService API Query
     let queryInterval = 1;
     let offsetInterval = 0;
-    let qValue = queryInterval*100;
     let targetArray;
 
-function fillAllQs(){
-    // let qValue = queryInterval*100;
-    for (var i = 1; i <= 10 ; i++){
-        qValue = i*100;
-        grabQuestions();
-    }
-}
-fillAllQs();
-
+    grabQuestions();
 //API Functions
 function grabQuestions(){
+    let qValue = queryInterval*100;
+    //Determine which 
     if(qValue === 100){
         targetArray = used100Questions;
     } else {
@@ -60,20 +49,14 @@ function grabQuestions(){
                         if(qValue === 600){
                             targetArray = used600Questions;
                         } else {
-                            if(qValue === 700){
-                                targetArray = used700Questions;
+                            if(qValue === 800){
+                                targetArray = used800Questions;
                             } else {
-                                if(qValue === 800){
-                                    targetArray = used800Questions;
+                                if(qValue === 1000){
+                                    targetArray = used1000Questions;
                                 } else {
-                                    if(qValue === 900){
-                                        targetArray = used900Questions;
-                                    } else {
-                                        if(qValue === 1000){
-                                            targetArray = used1000Questions;
-                                        } else {
-                                            
-                                        }}}}}}}}}}
+                                        
+                                    }}}}}}}}
 
 //Determine the offset for the jservice search query.
     if(targetArray === 96){
@@ -82,7 +65,7 @@ function grabQuestions(){
 
     const queryURL = "http://jservice.io/api/clues/?value=" + qValue + "&&offset=" + offsetInterval;
     $.get(queryURL, function(response){
-        qValue = queryInterval*100
+        console.log(queryInterval);
         console.log(qValue);
         for (var j = 1 ; j <= 6; j++){
             let randomQ;
@@ -90,13 +73,13 @@ function grabQuestions(){
             function randomSelectQ(){
                 randomQ = response[Math.floor(Math.random() * 100)];
                 targetId = randomQ.id;
-                if(!randomQ || targetArray.includes(targetId)){
-                    // console.log("we got a ringer! - " + randomQ.id);
+                if(targetArray.includes(targetId)){
+                    console.log("we got one! - " + targetId);
                     randomSelectQ();
+                } else{
+                    return randomQ
                 }
-                return randomQ
             }
-            console.log(qValue)
             qObjName = "val"+qValue+"Num"+j;
             tempQ = {};
             tempQ[qObjName] = {
@@ -106,15 +89,22 @@ function grabQuestions(){
                 value: randomQ.value,
                 airdate: randomQ.airdate,
                 category: randomQ.category.title,
+                imgUrl: "",
             }
             $.extend(currentQuestions[qValue], tempQ);
-            // console.log(currentQuestions[qValue])
             targetArray.push(randomQ.id)
         }
-        ++queryInterval
-        });
-        queryInterval=1;
+        if(queryInterval < 10){
+            ++queryInterval
+        }
+        if(queryInterval === 7 || queryInterval === 9){
+            ++queryInterval
+        }
         console.log(currentQuestions)
+        if(qValue < 1000){
+            grabQuestions();
+        }
+        });
     };
 
 
